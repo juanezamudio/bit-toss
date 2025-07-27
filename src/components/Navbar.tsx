f21ebe1b6e746badb44f9256a359a94479629de7
Juan Zamudio <jzamudio14@gmail.com>
2025-07-27 22:48:22 +0000

    finishing up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <a href="/">BitToss</a>
       </div>
-      <div className="navbar-menu">
-        <a href="/">Home</a>
-        <a href="/explorer">Explorer</a>
+      <button
+        className="navbar-hamburger"
+        aria-label="Toggle navigation"
+        onClick={() => setMenuOpen((open) => !open)}
+      >
+        <span className="hamburger-bar" />
+        <span className="hamburger-bar" />
+        <span className="hamburger-bar" />
+      </button>
+      <div className={`navbar-menu${menuOpen ? ' open' : ''}`}>
+        <a href="/" onClick={() => setMenuOpen(false)}>Home</a>
+        <a href="/explorer" onClick={() => setMenuOpen(false)}>Explorer</a>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
